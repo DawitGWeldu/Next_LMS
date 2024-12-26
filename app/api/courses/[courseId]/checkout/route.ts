76dd4e1551b99dba10f2091cdb08bb1d13706d5b
Dawit Getachew <gdawit35@gmail.com>
2024-12-26 10:13:17 +0300

    fix: update return and callback URLs for course checkout and enhance transaction handling in GET callback

--- app/api/courses/[courseId]/checkout/route.ts
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -47,8 +47,8 @@ export async function POST(
 
 
   const tx_reference = uuidv4();;
-  const return_url = `${process.env.NEXT_PUBLIC_APP_URL}/api/courses/checkout`;
-  const callback_url = `${process.env.NEXT_PUBLIC_APP_URL}/api/courses/checkout`;
+  const return_url = `${process.env.NEXT_PUBLIC_APP_URL}/courses/${params.courseId}/chapters/${chapter?.id}`;
+  const callback_url = `${process.env.NEXT_PUBLIC_APP_URL}/api/courses/${params.courseId}/checkout`;
 
   let checkout_url = null;
   const res = await axios({
@@ -86,30 +86,30 @@ export async function POST(
 export async function GET(req: Request) {
   const url = new URL(req.url);
   const searchParams = url.searchParams;
-  // console.log("[CALLBACK RAN]: [status]? ", url, searchParams);
+  console.log("[CALLBACK RAN]: [status]? ", url, searchParams);
 
-  // const tx_ref = searchParams.get('trx_ref')!;
-  //   try {
-  //     const transaction = await db.chapaTransaction.findFirst({
-  //       where: {
-  //         tx_ref: tx_ref,
-  //         status: 'PENDING'
-  //       }
-  //     });
-  //     console.log("[TRANSACTION]: ", JSON.stringify(transaction));
+  const tx_ref = searchParams.get('trx_ref')!;
+    try {
+      const transaction = await db.chapaTransaction.findFirst({
+        where: {
+          tx_ref: tx_ref,
+          status: 'PENDING'
+        }
+      });
+      console.log("[TRANSACTION]: ", JSON.stringify(transaction));
 
-  //     await db.purchase.create({
-  //       data: {
-  //         courseId: transaction!.courseId,
-  //         userId: transaction!.userId,
-  //       }
-  //     });
-  //     console.log("[CALLBACK RAN]: Success");
+      await db.purchase.create({
+        data: {
+          courseId: transaction!.courseId,
+          userId: transaction!.userId,
+        }
+      });
+      console.log("[CALLBACK RAN]: Success");
 
-  //     return NextResponse.json({ status: "Transaction Success" });
-  //   } catch (error) {
-  //     throw new Error("Transaction Data not found");
-  //   }
+      return NextResponse.json({ status: "Transaction Success" });
+    } catch (error) {
+      throw new Error("Transaction Data not found");
+    }
 
 
   // if (searchParams.has("status")) {
@@ -120,5 +120,5 @@ export async function GET(req: Request) {
     
   // }
 
-  return NextResponse.json({ message: "200", url: url || '', searchParams: searchParams || '' }, { status: 200 });
+  return NextResponse.json({ message: "200" }, { status: 200 });
 }
